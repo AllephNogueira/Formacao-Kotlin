40ae16149af56cfb558f55d456c64a04308569ad
Alleph Nogueira <126208329+AllephNogueira@users.noreply.github.com>
2024-07-13 16:20:17 -0300

    Add files via upload

---
+++ 01 - Linguagem Kotlin/AluGames/src/main/kotlin/Main.kt
@@ -0,0 +1,9 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    val nome = "Alleph"
+
+    println("Bem vindo $nome")
+}
